7cf68e5708ad9ede0272b49981c34d2106280445
amanprime07 <amanprime07@gmail.com>
2024-01-17 11:27:55 +0530

    added binary converter

---
+++ src/main/java/com/training/util/BinaryConverter.java
@@ -0,0 +1,33 @@
+package com.training.util;
+
+public class BinaryConverter {
+    long bits;
+    BinaryConverter(long bits) {
+        this.bits = bits;
+    }
+
+    String convertToBinary(long number) {
+        StringBuilder str = new StringBuilder();
+        var mask = 1L;
+        mask = mask << bits-1;
+        int spacer = 0;
+      for(;mask != 0L; mask >>>= 1){
+          if((number & mask) !=0){
+              str.append("1");
+          } else {
+              str.append("0");
+          }
+          if(spacer == 8) {
+              str.append(" ");
+              spacer = 0;
+          }
+          spacer++;
+      }
+      return str.toString();
+    }
+
+    public static void main(String[] args) {
+        var converter = new BinaryConverter(64);
+        System.out.println(converter.convertToBinary(1023));
+    }
+}
